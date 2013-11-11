1fac2d9b7edebc09c6b12de24fc54dee24d86d5b
Matt <hello@mattnull.com>
2013-11-10 22:57:05 -0700

    simple shell script to set environment variables and run node

---
+++ env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Set your environments variables
+envVars=(
+	"DB=mongodb://localhost/"
+	"PORT=3000"
+)
+
+setEnv () {
+	cmd=""
+
+	for i in "${envVars[@]}"
+	do
+		:
+		cmd+=$i
+		cmd+=" "
+	done
+
+	cmd+="node npm.js"
+
+	eval $cmd
+	exit 0
+}
+
+setEnv
+
+exit $?
